04d8718b317e5f710102ee31a53802c7270622da
rokkkk1125 <rokkkk1125@naver.com>
2022-07-13 16:47:44 +0900

    220713

---
+++ src/Ch04Scanner/C05Scanner.java
@@ -0,0 +1,70 @@
+package Ch04Scanner;
+
+import java.util.Scanner;
+
+public class C05Scanner {
+
+	public static void main(String[] args) {
+		//이름을 입력하세요 : 홀길동
+		//홍길동 님의 나이를 입력하세요 : 34
+		//홍길동 님의 주소를 입력하세요 : ㄴㅁㅇㄹㄴㅁㄹㅇ
+		
+		//[PROFILE]
+		//이름 : 홀길동
+		//나이 : 34
+		//주소 : ㅁㅈㄷㄱ
+		
+		Scanner sc = new Scanner(System.in);
+		
+//		System.out.print("이름을 입력하세요 : ");
+//		String str1 = sc.nextLine();
+//		System.out.print(str1+" 님의 나이를 입력하세요 : ");
+//		int num1 = sc.nextInt();
+//		System.out.print(str1+" 님의 주소를 입력하세요 : ");
+//		sc.nextLine();
+//		String str2 = sc.nextLine();
+//		
+//		sc.close();
+//		
+//		System.out.println("[PROFIEL]");
+//		System.out.printf("이름 : %s\n",str1);
+//		System.out.printf("나이 : %d\n",num1);
+//		System.out.printf("주소 : %s\n",str2);
+
+		//1번
+		
+//		System.out.print("첫번째 수 : ");
+//		int strNum1 = sc.nextInt();
+//		
+//		System.out.print("두번쨰 수 : ");
+//		int strNum2 = sc.nextInt();
+//		
+//		sc.close();
+//		
+//		int num1 = strNum1;
+//		int num2 = strNum2;
+//		int result = num1 + num2;
+//		System.out.println("덧셈 결과 : "+result);
+		
+		//2번
+		
+//		System.out.println("[필수 정보 입력]");
+//		System.out.print("1. 이름 : ");
+//		String str1 = sc.nextLine();
+//		System.out.print("2. 주민번호 6자리 : ");
+//		int num2 = sc.nextInt();		
+//		System.out.print("3. 전화번호 : ");
+//		sc.nextLine();
+//		String str3 = sc.nextLine();
+//		
+//		sc.close();
+//		
+//		System.out.println("[입력한 내용]");
+//		System.out.println("이름 : "+str1);
+//		System.out.println("주민번호 : "+num2);
+//		System.out.println("PH : "+str3);
+//		
+		
+	}
+
+}
