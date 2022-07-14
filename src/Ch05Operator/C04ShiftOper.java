b2bbb62e0dbf0a7b4a7fbc40459aeb1dbdf3fa79
rokkkk1125 <rokkkk1125@naver.com>
2022-07-14 16:49:50 +0900

    220714

---
+++ src/Ch05Operator/C04ShiftOper.java
@@ -0,0 +1,16 @@
+package Ch05Operator;
+
+public class C04ShiftOper {
+
+	public static void main(String[] args) {
+		
+		int num1 = 15;
+		int num2 = 20;
+		int num3 = num1<<num2;
+		int num4 = num1>>num2;
+		
+		System.out.println("num3 : "+num3);
+		System.out.println("num4 : "+num3);
+	}
+
+}
