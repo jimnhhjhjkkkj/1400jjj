2efce005918bfab257b87f6da56d1314a71ed48d
rokkkk1125 <rokkkk1125@naver.com>
2022-07-25 16:46:40 +0900

    220725

---
+++ src/Ch13Static/C02StaticMain.java
@@ -0,0 +1,21 @@
+package Ch13Static;
+
+import java.util.Scanner;
+
+class FruitStore{
+	static String list[]= {"사과","바나나","오렌지","자두"}
+}
+public class C02StaticMain {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("과일명 입력 : ");
+		String str = sc.next();
+		for(String f :FruitStore.list) {
+			if(F.equals(str)) {
+				System.out.println("해당과일이존재");
+				break;
+			}
+		}
+		System.out.println("종료함");
+	}
+}
